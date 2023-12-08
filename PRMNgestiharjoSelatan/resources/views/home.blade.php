fa32ef995dd560ce17bafce7c6f835cd06b55f09
aldirosyidin14 <mohammad1900018243@webmail.uad.ac.id>
2023-12-08 20:03:16 +0700

    update

---
+++ PRMNgestiharjoSelatan/resources/views/home.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <div>
+            <nav>
+                <p>Home</p>
+                <p>Kegiatan</p>
+                <p>Berita</p>
+                <div>
+                    <p>Login</p>
+                </div>
+            </nav>
+        </div>
+
+        <div>
+            <div>
+                <img src="" alt="">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
